548f93ae898aab18f5b1cbcf0fa88968891a8a67
Adam <a3aranha@gmail.com>
2021-03-31 15:14:34 -0400

    PORT change for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const exphbs = require('express-handlebars');
 const path = require('path')
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3306;
 
 // Body Parser middleware
 app.use(express.json());
